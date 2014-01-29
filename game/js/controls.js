a66ebb994462b042baa7868a8abe4107e91bcd49
pingbattlespl0x <Nathan.C.Shih@gmail.com>
2014-01-29 01:51:23 -0600

    see previous message, updated wrong directory

--- game/js/controls.js
+++ game/js/controls.js
@@ -31,15 +31,37 @@ function move() {
 function onDocumentMouseMove( event ) {
     event.preventDefault();
 
+
     mouse2D.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse2D.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
 
-function onDocumentMouseDown( event ) {
-    event.preventDefault();
+function onDocumentKeyDown( event ) {
+    switch( event.keyCode ) {
+        case 16: isShiftDown = true; break;
+        case 17: isCtrlDown = true; break;
+        case 187: isEqualsDown = true; break;
+        case 189: isDashDown = true; break;
+        case 65: aDown = true;  
+        rollOverMesh.position.x -= 50;
+        camera.position.x -= 50; break;
+        case 87: wDown = true;
+        //TODO w 
+        break;
+        case 83: sDown = true;
+        //TODO s
+        break;
+        case 68: dDown = true;
+        //TODO d
+        rollOverMesh.position.x += 50;
+        camera.position.x += 50; break;
+        //TODO WDS, and camera following the rolloverMesh
+        break;
 
-    var intersects = raycaster.intersectObjects( scene.children );
+
+        case 32: isSpaceDown = true; 
+        var intersects = raycaster.intersectObjects( scene.children );
 
     if ( intersects.length > 0 ) {
 
@@ -81,14 +103,7 @@ function onDocumentMouseDown( event ) {
             block_list.push(voxel);
         }
     }
-}
 
-function onDocumentKeyDown( event ) {
-    switch( event.keyCode ) {
-        case 16: isShiftDown = true; break;
-        case 17: isCtrlDown = true; break;
-        case 187: isEqualsDown = true; break;
-        case 189: isDashDown = true; break;
     }
 }
 
@@ -98,6 +113,8 @@ function onDocumentKeyUp( event ) {
         case 17: isCtrlDown = false; break;
         case 187: isEqualsDown = false; break;
         case 189: isDashDown = false; break;
+        case 65: aDown = false; break;
+        case 32: isSpaceDown = false; break;
     }
 }
 
@@ -108,73 +125,3 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-
-function includePointerLockControls() {
-  if ('pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document) {
-        var element = document.body;
-
-        var pointerlockchange = function ( event ) {
-            if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-                controls.enabled = true;
-                blocker.style.display = 'none';
-            } else {
-                controls.enabled = false;
-
-                blocker.style.display = '-webkit-box';
-                blocker.style.display = '-moz-box';
-                blocker.style.display = 'box';
-
-                instructions.style.display = '';
-            }
-        }
-
-        var pointerlockerror = function ( event ) {
-            instructions.style.display = '';
-        }
-
-        // Hook pointer lock state change events
-        document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-        document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-        document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-
-        document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-        document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-        document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-
-        instructions.addEventListener( 'click', function ( event ) {
-            instructions.style.display = 'none';
-
-            // Ask the browser to lock the pointer
-            element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-
-            if ( /Firefox/i.test( navigator.userAgent ) ) {
-
-                var fullscreenchange = function ( event ) {
-
-                    if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
-
-                        document.removeEventListener( 'fullscreenchange', fullscreenchange );
-                        document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
-
-                        element.requestPointerLock();
-                    }
-
-                }
-
-                document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-                document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-
-                element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
-
-                element.requestFullscreen();
-
-            } else {
-                element.requestPointerLock();
-            }
-
-        }, false );
-
-    } else {
-        instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-    }
-}
