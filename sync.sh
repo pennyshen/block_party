00377624b2a7ef5b2db07d016e93330a75067388
Penguinator <jenshenpenguin@gmail.com>
2014-04-04 02:46:07 -0500

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-git checkout gh-pages # go to the gh-pages branch
-git rebase master # bring gh-pages up to date with master
-git push origin gh-pages # commit the changes
-git checkout master # return to the master branch
+# git checkout gh-pages # go to the gh-pages branch
+# git rebase master # bring gh-pages up to date with master
+# git push origin gh-pages # commit the changes
+# git checkout master # return to the master branch
 
 
 # http://stackoverflow.com/questions/9646167/clean-up-a-fork-and-restart-it-from-the-upstream
+git checkout gh-pages
+git reset --hard origin/master
+git push --force
+git checkout master
