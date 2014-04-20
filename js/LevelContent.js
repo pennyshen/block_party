3369a21ce43bee14851c6e8d52a0f34621e20b48
Penguinator <jenshenpenguin@gmail.com>
2014-04-20 18:31:36 -0500

    added picture for monster cube and fixed bleeding level problem

--- js/LevelContent.js
+++ js/LevelContent.js
@@ -119,7 +119,7 @@ LevelContent.levels[LevelContent.BIG_PIECES] = [
 		new Level(["2_cubed", "straight3", "short_L", "t_block", "Z_block", "V_block"], '[{"x":0,"y":0,"z":0},{"x":1,"y":0,"z":0},{"x":0,"y":0,"z":1},{"x":1,"y":0,"z":1},{"x":0,"y":1,"z":0},{"x":1,"y":1,"z":0},{"x":0,"y":1,"z":1},{"x":1,"y":1,"z":1},{"x":1,"y":0,"z":2},{"x":1,"y":1,"z":2},{"x":0,"y":1,"z":2},{"x":2,"y":1,"z":2},{"x":2,"y":2,"z":2},{"x":0,"y":0,"z":2},{"x":0,"y":2,"z":2},{"x":2,"y":0,"z":2},{"x":2,"y":0,"z":1},{"x":2,"y":1,"z":1},{"x":0,"y":2,"z":1},{"x":1,"y":2,"z":1},{"x":2,"y":2,"z":1},{"x":1,"y":2,"z":2},{"x":2,"y":0,"z":0},{"x":2,"y":1,"z":0},{"x":2,"y":2,"z":0},{"x":1,"y":2,"z":0},{"x":0,"y":2,"z":0}]')
 ];
 LevelContent.LevelTypetoDescriptor[LevelContent.BIG_PIECES] = "Make a cube (Warning: HARD)";
-LevelContent.worlds[LevelContent.BIG_PIECES] = new World('space2.jpg', 'chiptune.mp3');
+LevelContent.worlds[LevelContent.BIG_PIECES] = new World('monster.jpg', 'chiptune.mp3');
 
 LevelContent.levels[LevelContent.THREE_D] = [
 		// 23
--- js/RandomMode.js
+++ js/RandomMode.js
@@ -6,6 +6,7 @@ function RandomMode() {
 	this.mode = Game.MODE_RANDOM;
 
 	hideAllInfo();
+	LevelContent.worlds[LevelContent.TUTORIAL].loadWorld();
 	showElement(showNextPiece_doc);
 }
 
@@ -24,6 +25,26 @@ RandomMode.prototype.getNextBlock = function() {
 	this.nextBlockName = getRandomMember(BlockGenerator.randomModeShapes);
 	nextPiece_doc.innerHTML = this.nextBlockName;
 	return toReturn;
+};
+
+RandomMode.prototype.scoreGame = function() {
+	var positionStrings = Object.keys(this.existingBlocks).splice(this.existingBlocks.length);
+	var positions, pos;
+	var axisMap = ['x', 'y', 'z']
+
+	positions = {};
+	for (var i = 0; i < axisMap.length; i++) {
+		positions[axisMap[i]] = [];
+	}
+
+	// get the positions from positionStrings into the positions map
+	for (var i = 0; i < positionStrings.length; i++) {
+		pos = positionStrings[i].split(',');
+		for (var j = 0; j < pos.length; j++) {
+			positions[axisMap[j]].push(parseInt(pos[j]));
+		}
+	}
+	console.log(positions);
 };
 
 RandomMode.prototype.endGame = function() {
