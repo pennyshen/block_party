3438c0deed91415b0d05de0aa994cace1a0971f1
Penguinator <jenshenpenguin@gmail.com>
2014-04-21 06:16:41 -0500

    aded ability to change goal shape color

--- js/World.js
+++ js/World.js
@@ -1,10 +1,15 @@
 
-function World(picture, music) {
+function World(picture, music, goalColor) {
 	this.picture = picture;
 	this.music = music;
+	this.goalColor = goalColor;
 }
 
 World.prototype.loadWorld = function() {
 	backgroundMesh.material.map = textures[this.picture];
 	mainMusic = sounds[this.music];
+	if (this.goalColor) {
+		game.preview.material.color.setHex(this.goalColor);
+		game.goalObject.material.color.setHex(this.goalColor);
+	}
 }
